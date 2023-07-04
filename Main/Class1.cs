62d28e2ad07604c562037bbe7145719dd54df9d3
316de <316de@315-DE-15>
2023-07-04 10:54:08 +0300

    Добавьте файлы проекта.

---
+++ Main/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building
+{
+
+    public class Building
+    {
+
+        public string Adress { get; set; }
+        public String Description { get; set; }
+        public List<Apartament> Apartaments = new List<Apartament>();
+        public Building(string adress, string descroption)
+        {
+            Adress = adress;
+            Description = descroption;
+
+
+        }
+        public void Show()
+        {
+            Console.WriteLine($"адрес = {Adress}, описание = {Description} ");
+        }
+
+        public List<Apartament> newList(Building building)
+        {
+            List<Apartament> col = new List<Apartament>();
+            foreach (Apartament b in building.Apartaments)
+            {
+                Apartament apartament = new Apartament(b.Owner, b.Number);
+                col.Add(apartament);
+            }
+            return col;
+        }
+
+        public void ShowAll(Building ps)
+        {
+
+            foreach (Apartament b in ps.Apartaments)
+            {
+                b.Show();
+            }
+        }
+
+
+    }
+
+    public class Apartament : IComparable<Apartament>
+    {
+        public int Number { get; set; }
+        public string Owner { get; set; }
+
+        public Apartament(string owner, int number)
+        {
+            Number = number;
+            Owner = owner;
+
+        }
+        public override string ToString()
+        {
+            string s = $"Данные о апартаментах: \n" +
+                       $"- номер: {Number} \n" +
+                       $"- владелец: {Owner} \n";
+
+            return s;
+        }
+        public int CompareTo(Apartament other)
+        {
+            if (this.Number < other.Number)
+            {
+                return -1;
+            }
+            else if (this.Number > other.Number)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Номер = {Number}, Владелец = {Owner}");
+        }
+    }
+
+
+}
+
---
+++ Main/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Building;
+
+//Пасмурнов
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Building.Building Apartament = new Building.Building("Г. Москва", "Кирпичный дом, 5 этажей");
+
+
+            Apartament apar1 = new Apartament("Виталя", 52);
+            Apartament.Apartaments.Add(apar1);
+            Apartament apar2 = new Apartament("Алексей", 32);
+            Apartament.Apartaments.Add(apar2);
+            Apartament apar3 = new Apartament("Богдан", 42);
+            Apartament.Apartaments.Add(apar3);
+
+            Apartament.Show();
+            Console.WriteLine();
+
+            Apartament.ShowAll(Apartament);
+            Console.WriteLine();
+            List<Apartament> col = Apartament.newList(Apartament);
+            col.Sort();
+
+            foreach (Apartament lib in col)
+            {
+                Console.WriteLine(lib.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
